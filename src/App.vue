4f1d6dd732961b9067acb4ae9216a79c0d4107bb
安河桥北 <2680484799@qq.com>
2020-12-21 17:06:13 +0800

    封装组件

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <mainTabBar> </mainTabBar>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import mainTabBar from "./components/tabbar/mainTabBar";
+
+// import tabBar from "./components/tabbar/tabbar";
+// import tabItem from "./components/tabbar/tabBar_Item";
+
+export default {
+  data() {
+    return {
+      current: ""
+    };
+  },
+
+  components: {
+    mainTabBar
+  }
+};
+</script>
+
+<style lang="less">
+@import "./assets/css/normalize.css";
+
+.active {
+  color: red;
+}
+</style>
---
+++ src/components/tabbar/mainTabBar.vue
@@ -0,0 +1,61 @@
+<!--  -->
+<template>
+  <div>
+      <!-- @在jsconfig 文件里配置了 代表src -->
+    <tabBar>
+      <tabItem path="/home" activeColor="red">
+        <img slot="item-icon" src="@/assets/img/tabBar/home.svg" />
+        <img slot="item-active" src="@/assets/img/tabBar/home_active.svg" />
+        <p slot="item-text">
+          首页
+        </p>
+      </tabItem>
+      <tabItem path="/category">
+        <img slot="item-icon" src="@/assets/img/tabBar/category.svg" />
+        <img
+          slot="item-active"
+          src="../../assets/img/tabBar/category_active.svg"
+        />
+        <p slot="item-text">
+          分类
+        </p>
+      </tabItem>
+      <tabItem path="/cart">
+        <img slot="item-icon" src="@/assets/img/tabBar/shopcart.svg" />
+        <img
+          slot="item-active"
+          src="@/assets/img/tabBar/shopcart_active.svg"
+        />
+        <p slot="item-text">
+          购物车
+        </p>
+      </tabItem>
+      <tabItem path="/profile">
+        <img slot="item-icon" src="@/assets/img/tabBar/profile.svg" />
+        <img
+          slot="item-active"
+          src="@/assets/img/tabBar/profile_active.svg"
+        />
+        <p slot="item-text">
+          我的
+        </p>
+      </tabItem>
+    </tabBar>
+  </div>
+</template>
+
+<script>
+import tabBar from "./tabbar.vue";
+import tabItem from "./tabBar_Item.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    tabBar,
+    tabItem,
+  },
+};
+</script>
+<style lang="less" scoped></style>
